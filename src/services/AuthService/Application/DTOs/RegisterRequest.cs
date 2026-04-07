271308eb338f32c16fbdb4bb879de9cdc50503d7
Roshan Chapagain <arobce@gmail.com>
2026-04-07 10:55:43 -0500

    Updates auth service to take firstname and lastname instead of fullname

--- src/services/AuthService/Application/DTOs/RegisterRequest.cs
+++ src/services/AuthService/Application/DTOs/RegisterRequest.cs
@@ -4,6 +4,7 @@ public class RegisterRequest
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public string FullName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
 }
